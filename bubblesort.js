ca2df68d32b960f2044fdca5de809c5a3c415b24
Kristine Lee <kristine0105@gmail.com>
2020-07-22 14:59:08 -0400

    separated swap() from bubbleSort()

--- bubblesort.js
+++ bubblesort.js
@@ -1,14 +1,26 @@
-function bubbleSort() {
-  function swap() {
-    let array = [4, 5, 9, 2];
-    array.sort(function (x, y) {
-      return x - y;
-    });
-    console.log(array);
-  }
+function swap(array){
+  const sorted = array.sort((x,y) => {return x-y})
+  console.log(sorted)
 }
-bubbleSort();
-console.log(bubbleSort);
+
+function bubbleSort(array) {
+  return swap(array)
+  // function swap() {
+  //   let array = [4, 5, 9, 2];
+  //   array.sort(function (x, y) {
+  //     return x - y;
+  //   });
+  //   console.log(array);
+  // }
+}
+
+//bubbleSort();
+//console.log(bubbleSort);
+
+let thisArray = [4, 5, 9, 2]
+const answer = bubbleSort(thisArray);
+console.log(answer);
+
 
 // const len = array.length;
 // const tempArray = [];
